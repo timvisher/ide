f1db22d724188807a791dd8ab1226f02be62c322
Tim Visher <tvisher@rjmetrics.com>
2017-03-03 15:41:40 -0500

    Add boxcutter bash functions

---
+++ bash/functions/boxcutter.bash
@@ -0,0 +1,9 @@
+boxcutter_optimization_queue() {
+    if [[ -z $1 ]]
+    then
+        curl --silent http://172.16.10.202:8080/job/Boxcutter%20--%20Rebuild/lastSuccessfulBuild/artifact/artifacts/chef-run-report.json \
+            | jq '.all_resources | group_by(.json_class) | [ .[] | reduce .[] as $i ({json_class: "", elapsed_time: 0}; {json_class: $i.json_class, elapsed_time: (.elapsed_time + $i.instance_vars.elapsed_time)})] | sort_by(.elapsed_time) | reverse | .[:10]'
+    else
+        jq '.all_resources | group_by(.json_class) | [ .[] | reduce .[] as $i ({json_class: "", elapsed_time: 0}; {json_class: $i.json_class, elapsed_time: (.elapsed_time + $i.instance_vars.elapsed_time)})] | sort_by(.elapsed_time) | reverse | .[:10]' < "$1"
+    fi
+}
