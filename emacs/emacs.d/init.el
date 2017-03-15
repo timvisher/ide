86abfd904acd2e5bbc8297c21a7691d49d2d8123
Tim Visher <tvisher@rjmetrics.com>
2017-03-15 10:57:20 -0400

    magit 2.10.3

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -484,7 +484,14 @@ again."
  '(global-whitespace-mode nil)
  '(hippie-expand-try-functions-list
    (quote
-    (try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol)))
+    (try-complete-file-name-partially
+     try-complete-file-name
+     try-expand-all-abbrevs
+     try-expand-dabbrev
+     try-expand-dabbrev-all-buffers
+     try-expand-dabbrev-from-kill
+     try-complete-lisp-symbol-partially
+     try-complete-lisp-symbol)))
  '(ido-ubiquitous-mode t)
  '(ido-vertical-mode t)
  '(js-indent-level 2)
