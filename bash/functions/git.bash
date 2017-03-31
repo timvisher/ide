16f84063c1a6e56b562732a60b19a995ee3b27a6
Tim Visher <tim@stitchdata.com>
2017-03-31 10:21:51 -0400

    Pairing functions

---
+++ bash/functions/git.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+pair_show() {
+    git config --global user.name
+    git config --global user.email
+}
+
+pair_set() {
+    if (( 2 != $# )) || [[ $1 != *' and '* ]] || [[ $2 != *+*@stitchdata.com ]]
+    then
+        echo "# pair_set 'First Lass and First Lass' 'first+first@stitchdata.com'" >&2
+        return 1
+    fi
+
+    git config --global user.name "$1"
+    git config --global user.email "$2"
+
+    pair_show
+}
+
+pair_unpair() {
+    if (( 2 != $# )) || [[ $1 == *' and '* ]] || ! [[ $2 =~ ^[^+]+@stitchdata.com ]]
+    then
+        echo "# unpair 'First Last' 'first@stitchdata.com'" >&2
+    fi
+
+    git config --global user.name "$1"
+    git config --global user.email "$2"
+
+    pair_show
+}
+
