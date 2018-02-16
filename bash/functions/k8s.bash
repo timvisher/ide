cd4134e7749877259834e102cbcb517c3b1f2a1d
Tim Visher <tim@stitchdata.com>
2018-02-16 10:35:18 -0500

    Add k8s_ssh_node_instances

--- bash/functions/k8s.bash
+++ bash/functions/k8s.bash
@@ -6,6 +6,10 @@ k8s_kubectl_shell() {
     ssh -t "$(aws_bastion1_ip)" sudo -u psantaclara -i bash -l
 }
 
-ssh_k8s_instances() {
+k8s_ssh_node_instances() {
     _aws_as_describe_groups_instances 'nodes.kube.stitchdata.com' | jq -r '"ssh admin@\(.PrivateIpAddress)"'
+}
+
+ssh_k8s_instances() {
+    k8s_ssh_node_instances
 }
