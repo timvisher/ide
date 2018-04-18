db97cb8fb2c9dfaf968f54244bcf45c6d6fa6813
Tim Visher <tim@stitchdata.com>
2018-04-18 11:23:59 -0400

    Add ide_stats_service_connect_db

--- bash/functions/connections_service.bash
+++ bash/functions/connections_service.bash
@@ -1,3 +1,3 @@
 ide_connections_service_connect_db() {
-    ssh_connection_service_instance -t connect-db
+    ssh_connection_service_instance -t connect-db "$@"
 }
---
+++ bash/functions/stats_service.bash
@@ -0,0 +1,3 @@
+ide_stats_service_connect_db() {
+    ssh_stats_service_instance -t connect-db "$@"
+}
