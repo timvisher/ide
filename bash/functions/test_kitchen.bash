6c46ad0aee5640507f94e74ae66d24380b305cea
Tim Visher <tim@stitchdata.com>
2018-05-17 12:34:42 -0400

    Fix ide_test_kitchen_list_instances

--- bash/functions/test_kitchen.bash
+++ bash/functions/test_kitchen.bash
@@ -1,11 +1,6 @@
 ide_test_kitchen_list_instances() {
-    aws_ec2_describe_instances \
-        | jq 'select("test-kitchen" == (
-                  select(.Tags) |
-                  .Tags[] |
-                  select("created-by" == .Key) |
-                  .Value
-              )) |
-              select(.State.Name != "terminated") |
-              {InstanceId, LaunchTime}'
+    aws ec2 describe-instances \
+        --filters 'Name=tag:created-by,Values=test-kitchen' \
+        'Name=instance-state-name,Values=running' \
+        | jq '.Reservations[].Instances[] | {InstanceId,LaunchTime}'
 }
