26195ee32707cfe73457d222a7ce2924cfeddd9c
Tim Visher <tim@stitchdata.com>
2018-08-29 10:56:09 -0400

    Remove melpa and marmalade from package archives

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -7,12 +7,12 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/")
              t)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/")
-             t)
-(add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/")
-             t)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/")
+;;              t)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "https://marmalade-repo.org/packages/")
+;;              t)
 (add-to-list 'package-archives
              '("org" . "https://orgmode.org/elpa/")
              t)
