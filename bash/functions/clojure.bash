e764981f3cbe45df6df4ea105822788e61c1a261
Tim Visher <tim@stitchdata.com>
2018-10-03 08:48:43 -0400

    Finalize rebl

--- bash/functions/clojure.bash
+++ bash/functions/clojure.bash
@@ -1,4 +1,34 @@
+_rebl_translate_lein_dep() {
+    local lein_dep lein_dep_name lein_dep_version
+
+    lein_dep=$1
+
+    IFS=' ' read -r lein_dep_name lein_dep_version <<<"$lein_dep"
+
+    lein_dep_name="${lein_dep_name/[/}"
+    lein_dep_version="${lein_dep_version/]/}"
+
+    echo "${lein_dep_name} {:mvn/version ${lein_dep_version}}"
+}
+
+_rebl_assert_preconditions() {
+    if ! [[ $(type -t clojure) == file ]]
+    then
+        echo '`clojure` not installed.' \
+             'https://clojure.org/guides/getting_started' >&2
+        return 1
+    fi
+}
+
 rebl() {
-    echo 'ALPHA' >&2
-    clojure -Sdeps "{:deps {com.bhauman/rebel-readline {:mvn/version \"0.1.3\"}}}" -m rebel-readline.main
+    _rebl_assert_preconditions || return 1
+    local -a deps
+    while [[ -n $1 ]]
+    do
+        read -r line < <(_rebl_translate_lein_dep "$1")
+        shift
+        deps+=($line)
+    done
+    clojure -Sdeps '{:deps {com.bhauman/rebel-readline {:mvn/version "0.1.4"} '"${deps[*]}"'}}' \
+            -m rebel-readline.main
 }
