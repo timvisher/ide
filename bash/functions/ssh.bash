a79f341b64ec08a3ce71d04d4fc8c5ceb655e85c
Tim Visher <tim@stitchdata.com>
2019-08-02 12:21:38 -0400

    Add ide_ssh_regenerate_stitch_opsworks_config
    This creates a file `~/.ssh/config.d/stitch_opsworks` which, if `Include`d
    in your ssh_config, will properly resolve all current opsworks boxes.

--- bash/functions/ssh.bash
+++ bash/functions/ssh.bash
@@ -1,3 +1,15 @@
 delete_known_host_line () {
     sed -i".bak$(date -u '+%FT%TZ')" "$1"'d' ~/.ssh/known_hosts
 }
+
+ide_ssh_regenerate_stitch_opsworks_config() {
+  mkdir -p ~/.ssh/config.d
+  while read -r stack_id
+  do
+    aws opsworks describe-instances --stack-id "$stack_id"
+  done < <(stack_ids) \
+    | > ~/.ssh/config.d/stitch_opsworks \
+        jq -r '.Instances[]
+               | select(.PrivateIp)
+               | "Host \(.Hostname)\n  HostName \(.PrivateIp)\n"'
+}
