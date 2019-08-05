c8c728c78c983b0bd0bb12c3c7b5bb45647612c5
Tim Visher <tim@stitchdata.com>
2019-08-05 16:41:36 -0400

    Add ide_ssh_regenerate_stitch_opsworks_layer_instance_config

--- bash/functions/ssh.bash
+++ bash/functions/ssh.bash
@@ -8,3 +8,29 @@ ide_ssh_regenerate_stitch_opsworks_config() {
     | jq -r '"Host \(.Hostname)\n  HostName \(.PrivateIp)\n"' \
     | tee ~/.ssh/config.d/stitch_opsworks
 }
+
+ide_ssh_regenerate_stitch_opsworks_layer_instance_config() {
+  mkdir -p ~/.ssh/config.d
+  local layer_names_by_id
+  layer_names_by_id="$(ide_aws_opsworks_describe_layers \
+                         | jq -s 'reduce .[] as $item ({};
+                                    . + {
+                                          ($item|.LayerId):
+                                          ($item|.Name)
+                                        })'
+                     )"
+  ide_aws_opsworks_describe_instances \
+    | jq -r -s 'map(select(.PrivateIp))
+                | group_by(.LayerIds[0])[]
+                | .[0]
+                | {
+                    layer:
+                    '"$layer_names_by_id"'[.LayerIds[0]],
+
+                    PrivateIp
+                  }
+                # Must remove bastion to prevent infinite ProxyJump loop
+                | select(.layer != "bastion")
+                | "Host \(.layer)\n  HostName \(.PrivateIp)\n"' \
+    | tee ~/.ssh/config.d/stitch_opsworks_layer_instance
+}
