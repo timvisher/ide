8e373b340950f7e703990e8414cf2610885e1900
Tim Visher <tim@stitchdata.com>
2019-08-16 11:57:13 -0400

    Also append stitch- to generated ssh config hostnames

--- bash/functions/ssh.bash
+++ bash/functions/ssh.bash
@@ -5,7 +5,7 @@ delete_known_host_line () {
 ide_ssh_regenerate_stitch_opsworks_config() {
   mkdir -p ~/.ssh/config.d
   ide_aws_opsworks_instance_ips \
-    | jq -r '"Host \(.Hostname)\n  HostName \(.PrivateIp)\n"' \
+    | jq -r '"Host stitch-\(.Hostname | gsub("_"; "-"))\n  HostName \(.PrivateIp)\n"' \
     | tee ~/.ssh/config.d/stitch_opsworks
 }
 
@@ -13,11 +13,10 @@ ide_ssh_regenerate_stitch_opsworks_layer_instance_config() {
   mkdir -p ~/.ssh/config.d
   local layer_names_by_id
   layer_names_by_id="$(ide_aws_opsworks_describe_layers \
-                         | jq -s 'reduce .[] as $item ({};
-                                    . + {
-                                          ($item|.LayerId):
-                                          ($item|.Name)
-                                        })'
+                         | jq -s '[
+                                    .[] | {(.LayerId): .Name}
+                                  ]
+                                  | add'
                      )"
   ide_aws_opsworks_describe_instances \
     | jq -r -s 'map(select(.PrivateIp))
@@ -31,6 +30,11 @@ ide_ssh_regenerate_stitch_opsworks_layer_instance_config() {
                   }
                 # Must remove bastion to prevent infinite ProxyJump loop
                 | select(.layer != "bastion")
-                | "Host \(.layer)\n  HostName \(.PrivateIp)\n"' \
+                | "Host \(.layer | gsub("_"; "-")) stitch-\(.layer | gsub("_"; "-"))\n  HostName \(.PrivateIp)\n"' \
     | tee ~/.ssh/config.d/stitch_opsworks_layer_instance
+}
+
+ide_ssh_regenerate_stitch_all_config() {
+  ide_ssh_regenerate_stitch_opsworks_config
+  ide_ssh_regenerate_stitch_opsworks_layer_instance_config
 }
