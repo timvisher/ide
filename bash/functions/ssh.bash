a4097ede98bd72a5dd8dda6c18b88c64b096c8ee
Tim Visher <tim@stitchdata.com>
2019-09-03 13:17:46 -0400

    Add ide_ssh_regenerate_k8s_nodes_config

--- bash/functions/ssh.bash
+++ bash/functions/ssh.bash
@@ -34,7 +34,63 @@ ide_ssh_regenerate_stitch_opsworks_layer_instance_config() {
     | tee ~/.ssh/config.d/stitch_opsworks_layer_instance
 }
 
+ide_ssh_regenerate_k8s_nodes_config() {
+  mkdir -p ~/.ssh/config.d
+  cat > ~/.ssh/config.d/staging-kube-bastion <<EOF
+Match host stitch-nodes*,stitch-*_eks*
+  ForwardAgent yes
+  ConnectTimeout 5
+  StrictHostKeyChecking no
+  LogLevel QUIET
+  UpdateHostKeys no
+  UserKnownHostsFile /dev/null
+
+Match host stitch-nodes*
+  User admin
+
+Host staging-kube-bastion
+  HostName bastion.staging-kube.stitchdata.com
+  ServerAliveInterval 50
+  User admin
+
+Match host stitch-*_eks*
+  ProxyJump staging-kube-bastion
+  User ec2-user
+EOF
+
+  local node_groups=(
+    'nodes.kube.stitchdata.com'
+    'nodes.staging-kube.stitchdata.com'
+    'production_eks'
+    'staging_eks'
+  )
+
+  aws_as_describe_groups_instances "${node_groups[@]}" \
+    | jq -r '
+{
+  PrivateIpAddress,
+  Name: "\(.Tags|from_entries|.Name|gsub("\\."; "-"))-\(.PrivateIpAddress|gsub("\\."; "-"))"
+}
+| "Host stitch-\(.Name)\n  HostName \(.PrivateIpAddress)\n"' \
+    | tee ~/.ssh/config.d/stitch_k8s_nodes
+
+  aws_as_describe_groups_instances "${node_groups[@]}" \
+    | jq -rs '
+[
+  .[]
+  | {
+      PrivateIpAddress,
+      Name: (.Tags|from_entries|.Name|gsub("\\."; "-"))
+    }
+]
+| group_by(.Name)[]
+| .[0]
+| "Host stitch-\(.Name)\n  HostName \(.PrivateIpAddress)\n"' \
+         | tee ~/.ssh/config.d/stitch_k8s_asgs
+}
+
 ide_ssh_regenerate_stitch_all_config() {
   ide_ssh_regenerate_stitch_opsworks_config
   ide_ssh_regenerate_stitch_opsworks_layer_instance_config
+  ide_ssh_regenerate_k8s_nodes_config
 }
