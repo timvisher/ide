d3af0070cef6bcc2041cf08d45421b0b5a32bc80
Tim Visher <tim@stitchdata.com>
2019-09-10 14:47:30 -0400

    Allow generating an /etc/hosts entries section for stitch hosts

--- bash/functions/ssh.bash
+++ bash/functions/ssh.bash
@@ -9,6 +9,12 @@ ide_ssh_regenerate_stitch_opsworks_config() {
     | tee ~/.ssh/config.d/stitch_opsworks
 }
 
+ide_hosts_opsworks_entries() {
+  mkdir -p ~/.ssh/config.d
+  ide_aws_opsworks_instance_ips |
+    jq -r '"\(.PrivateIp) stitch-\(.Hostname)"'
+}
+
 ide_ssh_regenerate_stitch_opsworks_layer_instance_config() {
   mkdir -p ~/.ssh/config.d
   local layer_names_by_id
@@ -32,6 +38,29 @@ ide_ssh_regenerate_stitch_opsworks_layer_instance_config() {
                 | select(.layer != "bastion")
                 | "Host stitch-\(.layer | gsub("_"; "-"))\n  HostName \(.PrivateIp)\n"' \
     | tee ~/.ssh/config.d/stitch_opsworks_layer_instance
+}
+
+ide_hosts_opsworks_layer_entries() {
+  local layer_names_by_id
+  layer_names_by_id="$(ide_aws_opsworks_describe_layers \
+                         | jq -s '[
+                                    .[] | {(.LayerId): .Name}
+                                  ]
+                                  | add'
+                     )"
+  ide_aws_opsworks_describe_instances |
+    jq -r -s 'map(select(.PrivateIp))
+              | group_by(.LayerIds[0])[]
+              | .[0]
+              | {
+                  layer:
+                  '"$layer_names_by_id"'[.LayerIds[0]],
+
+                  PrivateIp
+                }
+              # Must remove bastion to prevent infinite ProxyJump loop
+              | select(.layer != "bastion")
+              | "\(.PrivateIp) stitch-\(.layer | gsub("_"; "-"))"'
 }
 
 ide_ssh_regenerate_k8s_nodes_config() {
@@ -73,7 +102,7 @@ EOF
     | jq -r '
 {
   PrivateIpAddress,
-  Name: "\(.Tags|from_entries|.Name|gsub("\\."; "-"))-\(.PrivateIpAddress|gsub("\\."; "-"))"
+  Name: "\(.Tags|from_entries|.Name|gsub("[\\._]"; "-"))-\(.PrivateIpAddress|gsub("\\."; "-"))"
 }
 | "Host stitch-\(.Name)\n  HostName \(.PrivateIpAddress)\n"' \
     | tee ~/.ssh/config.d/stitch_k8s_nodes
@@ -84,7 +113,7 @@ EOF
   .[]
   | {
       PrivateIpAddress,
-      Name: (.Tags|from_entries|.Name|gsub("\\."; "-"))
+      Name: (.Tags|from_entries|.Name|gsub("[\\._]"; "-"))
     }
 ]
 | group_by(.Name)[]
@@ -93,8 +122,44 @@ EOF
          | tee ~/.ssh/config.d/stitch_k8s_asgs
 }
 
+ide_hosts_k8s_nodes_entries() {
+  local node_groups=(
+    'nodes.kube.stitchdata.com'
+    'nodes.staging-kube.stitchdata.com'
+    'production_eks'
+    'staging_eks'
+  )
+
+  aws_as_describe_groups_instances "${node_groups[@]}" \
+    | jq -r '
+{
+  PrivateIpAddress,
+  Name: "\(.Tags|from_entries|.Name|gsub("[\\._]"; "-"))-\(.PrivateIpAddress|gsub("\\."; "-"))"
+}
+| "\(.PrivateIpAddress) stitch-\(.Name)"'
+
+  aws_as_describe_groups_instances "${node_groups[@]}" \
+    | jq -rs '
+[
+  .[]
+  | {
+      PrivateIpAddress,
+      Name: (.Tags|from_entries|.Name|gsub("[\\._]"; "-"))
+    }
+]
+| group_by(.Name)[]
+| .[0]
+| "\(.PrivateIpAddress) stitch-\(.Name)"'
+}
+
 ide_ssh_regenerate_stitch_all_config() {
   ide_ssh_regenerate_stitch_opsworks_config
   ide_ssh_regenerate_stitch_opsworks_layer_instance_config
   ide_ssh_regenerate_k8s_nodes_config
+}
+
+ide_hosts_all_entries() {
+  ide_hosts_k8s_nodes_entries
+  ide_hosts_opsworks_layer_entries
+  ide_hosts_opsworks_entries
 }
