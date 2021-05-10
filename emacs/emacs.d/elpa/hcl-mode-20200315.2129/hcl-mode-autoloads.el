85a1c67d22335ec27e30c162e380533090e4468d
Tim Visher <tim@stitchdata.com>
2021-05-10 10:06:08 -0400

    hcl-mode: 20170107.27 → 20200315.2129

--- emacs/emacs.d/elpa/hcl-mode-20200315.2129/hcl-mode-autoloads.el
+++ emacs/emacs.d/elpa/hcl-mode-20200315.2129/hcl-mode-autoloads.el
@@ -1,9 +1,12 @@
 ;;; hcl-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "hcl-mode" "hcl-mode.el" (22746 25382 0 0))
+;;;### (autoloads nil "hcl-mode" "hcl-mode.el" (0 0 0 0))
 ;;; Generated autoloads from hcl-mode.el
 
 (autoload 'hcl-mode "hcl-mode" "\
@@ -15,11 +18,14 @@ Major mode for editing hcl configuration file
 
 (add-to-list 'auto-mode-alist '("\\.nomad\\'" . hcl-mode))
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "hcl-mode" '("hcl-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; hcl-mode-autoloads.el ends here
